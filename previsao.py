73718ed6f8a33da5914c56319f133086598185ac
laviniaaaf <102470754+laviniaaaf@users.noreply.github.com>
2024-10-17 21:35:43 -0300

    updt previsao.py

--- previsao.py
+++ previsao.py
@@ -40,28 +40,5 @@ def carregar_cidades(caminho_arquivo):
         print(f"Esse arquivo {caminho_arquivo} não foi encontrado!!!")
         return []
 
-def main():
-    parser = argparse.ArgumentParser(description = "Obter a previsão do tempo para cidades.")
-    parser.add_argument('--api_key', required = True, type = str, 
-                        help = 'Chave da API para a conexão com  o WeatherAPI.')
-    parser.add_argument('--arquivo', type = str, 
-                        help = 'Arquivo de texto contendo as cidades.')
-    parser.add_argument('--cidades', nargs = '+', 
-                        help = 'Lista das cidades para obter a previsão.')
-    args = parser.parse_args()
-
-    api_key = args.api_key
-
-    if args.arquivo:
-        cidades = carregar_cidades(args.arquivo)
-    elif args.cidades:
-        cidades = args.cidades
-    else:
-        print("Forneça um nome de arquivo com o '--arquivo' ou uma lista de cidades com '--cidades'.")
-        return
-
-    dados_previsao = obter_dados_previsao(cidades, api_key)
-    print(dados_previsao)
-
 if __name__ == "__main__":
     main()
